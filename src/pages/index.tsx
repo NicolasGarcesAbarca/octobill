c81c02d4daf436ace4e31215920bc759719d551c
Nicolas <nickogarces@gmail.com>
2023-05-22 20:07:38 -0400

    install react icons + responsive index layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { FcGoogle } from 'react-icons/fc'
 
 export default function Home() {
   return (
@@ -12,13 +9,20 @@ export default function Home() {
           <div><p className='text-white text-lg'>Login</p></div>
         </nav>
       </header>
-      <section className='lg:grid lg:grid-cols-2 lg:mt-[120px] px-8 lg:px-14'>
-        <div className='col-span-1 flex flex-col justify-center'>
-          <h1 className='text-gray-800 text-5xl font-semibold'>Ingresa y revisa tu consumo</h1>
-          <p className='text-gray-600 text-lg w-[490px] mt-4'>Hola, en octobill podras realizar cobros y revisar el estado de cuenta. </p>
+      <section className='px-8 lg:grid lg:grid-cols-2 lg:mt-[120px] lg:px-14'>
+        <div className='hidden lg:flex flex-col items-center py-10 md:p-0 col-span-1 '>
+          <div className='max-w-[600px]'>
+            <h1 className='text-gray-800  text-5xl  font-semibold '>Ingresa y revisa tu consumo</h1>
+            <p className=' text-gray-600 text-lg w-[490px] mt-4'>En octobill podrás revisar el estado de cuenta con gráficos de consumo y consultar dudas en el chat. </p>
+          </div>
+          <div className='grid grid-cols-3 gap-6 w-full py-6'>
+            <div className='col-span-1 aspect-square bg-transparent border rounded-lg'></div>
+            <div className='col-span-1 aspect-square bg-transparent border rounded-lg'></div>
+            <div className='col-span-1 aspect-square bg-transparent border rounded-lg'></div>
+          </div>
         </div>
-        <div className='col-span-1'>
-          <div className='bg-white border md:w-[420px] px-10 py-9 mx-auto rounded-lg'>
+        <div className='lg:col-span-1'>
+          <div className='bg-white border mt-16 lg:mt-0 md:w-[420px] px-10 py-8 lg:py-9 mx-auto rounded-lg'>
             <form className='flex flex-col justify-center '>
               <div className='flex flex-col gap-6'>
                 <div className='flex flex-col gap-1'>
@@ -40,16 +44,17 @@ export default function Home() {
                 </div>
               </div>
             </form>
-            <div className='mt-2'>
-              <button type="submit" className='w-full  px-4 py-2 rounded-lg border'>
+            <div className='mt-4'>
+              <button type="submit" className='flex items-center justify-center w-full  px-4 py-2 rounded-lg border hover:bg-gray-100'>
+                <FcGoogle size={30} className='pr-1 text-gray-700' />
                 <p className='text-gray-700 font-semibold'>Entrar con Google</p>
               </button>
             </div>
             <div className='mt-6'>
               <p className='text-sm text-center text-gray-600'>
-                Aun no estas registrado?
+                ¿Aún no estas registrado?
                 <span className='text-orange-700 text-sm font-medium ml-1 hover:underline cursor-pointer'>
-                  Registrate
+                  Regístrate
                 </span>
               </p>
 
